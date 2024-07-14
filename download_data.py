e0ab90147589508bdb25d17549a41f1108d195a4
Yin_Jun <yinjun_56@qq.com>
2024-07-14 11:56:42 +0800

    update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,5 +1,13 @@
 # automated downloading and extracting of data would be available after the work is publicly available
 # current use the private share link to manually download the data and unzip to the respective root (e.g., \SurFF)
 
-# download link for the data files (SurFF_DataFiles.zip)
-# https://figshare.com/s/f234fba96255b77788b0
+
+# download link for the core data files (SurFF_CoreDataFiles.zip)
+# This file contains the core data files for the SurFF project, allowing the user to run the SurFF prediction for intermetalic crystals.
+# Example usage can be found in the SurFF_Example.ipynb file.
+# https://figshare.com/s/82853778002cafffa59c (239MB)
+
+
+# download link for the all data files (SurFF_DataFiles.zip)
+# This file contains all the data files for the SurFF project, including additional DFT datasets generated.
+# https://figshare.com/s/517e78125f05b66f8e7d (4.6GB)
